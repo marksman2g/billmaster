4459ed8513d55cd252e5d4d6ea914c7d10365c61
BatchScribe Dev <computer.fieldtech@gmail.com>
2026-06-01 05:16:17 -0400

    Configure hosted Supabase publishable key

--- billmaster-config.js
+++ billmaster-config.js
@@ -1,5 +1,4 @@
 window.BILLMASTER_CLOUD_CONFIG = {
   url: "https://hhmiyebcduegpoihbgrt.supabase.co",
-  anonKey: ""
+  anonKey: "sb_publishable_f2-RP5j4yNFEvdbA6BpH5Q_DKcO6Z1Ssb_publishable_f2-RP5j4yNFEvdbA6BpH5Q_DKcO6Z1S"
 };
-
